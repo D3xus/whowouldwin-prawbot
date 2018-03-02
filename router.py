10f8df7f670e9de28da844f063420e96d7bee2de
D3xus <thed3xus@gmail.com>
2018-03-02 16:09:19 -0500

    Comment out bot endpoint for now

--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-from bot.flair_bot import FlairBot as FlairBot 
+#from bot.flair_bot import FlairBot as FlairBot 
 from flask import Flask, render_template
 
 # set the project root directory as the static folder, you can set others.
@@ -8,11 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/flairbot")
-def flairbotRoute():
-    print("hello")
-    FlairBot()
-    return "<h1>hi</h1>"
+#@app.route("/flairbot")
+#def flairbotRoute():
+#    print("hello")
+#    FlairBot()
+#    return "<h1>hi</h1>"
 
 if __name__ == "__main__":
     app.run()
