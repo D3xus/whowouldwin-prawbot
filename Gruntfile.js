1ca392c633f69b1944f16e798bf4834ef0feba3a
D3xus <thed3xus@gmail.com>
2018-03-02 21:50:28 -0500

    Bringing in Reddit

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,24 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     sass: {
-      dist: {
+      reddit: {
         options: {
-          // style: 'expanded', // makes the output CSS code human readable. 
+          // style: 'expanded', // makes the output CSS code human readable.
           style: 'compressed', // removes the line breaks and spaces to put the code all on one line.
           noCache: true
         },
         files: {
-            'static/flairbot.css' : 'src/sass/main.scss'
+          'reddit/reddit.css': 'src/sass_reddit/main.scss'
+        }
+      },
+      site: {
+        options: {
+          // style: 'expanded', // makes the output CSS code human readable.
+          style: 'compressed', // removes the line breaks and spaces to put the code all on one line.
+          noCache: true
+        },
+        files: {
+          'static/flairbot.css': 'src/sass_site/main.scss'
         }
       }
     },
@@ -48,37 +58,22 @@ module.exports = function(grunt) {
         tasks: ['sync']
       },
       scss: {
-        files: ['src/sass/**/*'],
+        files: ['src/sass_site/**/*'],
         tasks: ['sass']
       },
       grunt: {
         files: 'Gruntfile.js',
         tasks: ['build']
       }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 9000,
-          base: 'dist',
-          middleware: function(connect, options, middlewares) {
-            middlewares.unshift(function(req, res, next) {
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              next();
-            });
-            return middlewares;
-          }
-        }
-      }
     }
   });
-
-  // > grunt deploy - compiles project and sends to ftp
-  grunt.registerTask('deploy', ['build', 'ftp-deploy']);                
-  // > grunt build  - compiles project
-  grunt.registerTask('build', ['sass', 'sync']);    
-  // > grunt start  - compiles project, runs localhost server at http://localhost:9000 and re-builds project when files change
-  grunt.registerTask('start', ['build', 'connect', 'watch']);
+  
+  // > grunt build_reddit - compiles project for reddit's stylesheet
+  grunt.registerTask('build_reddit', ['sass:reddit']);
+  // > grunt build_site  - compiles project for the reddit chooser
+  grunt.registerTask('build_site', ['sass:site', 'sync']);    
+  // > grunt start  - compiles project and re-builds project when files change
+  grunt.registerTask('start', ['build', 'watch']);
   // > grunt        - compiles project (defining default task)
   grunt.registerTask('default', ['build']);
 }
